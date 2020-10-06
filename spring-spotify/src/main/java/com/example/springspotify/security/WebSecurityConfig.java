abd654e5895d02a81c79d255f1993462dc9f0e0b
karobarca <karobarca@gmail.com>
2020-10-06 14:57:37 +0200

    created project, set security properties, added security config class

---
+++ spring-spotify/src/main/java/com/example/springspotify/security/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.springspotify.security;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableOAuth2Sso
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // regulation of requests
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}
---
+++ spring-spotify/src/test/java/com/example/springspotify/SpringSpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springspotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
